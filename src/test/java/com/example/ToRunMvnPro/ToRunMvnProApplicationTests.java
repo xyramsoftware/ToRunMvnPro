10a5e49d34e0756e89587eb552e8b0ca98e4d32a
xyramsoftware <xyramsoftware@gmail.com>
2019-12-25 20:43:38 +0530

    Create ToRunMvnProApplicationTests.java

---
+++ src/test/java/com/example/ToRunMvnPro/ToRunMvnProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ToRunMvnPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToRunMvnProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
