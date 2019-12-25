f89acf920420743fcba3cabc43685747f08fedd5
xyramsoftware <xyramsoftware@gmail.com>
2019-12-25 20:43:46 +0530

    Create ToRunMvnProApplication.java

---
+++ src/main/java/com/example/ToRunMvnPro/ToRunMvnProApplication.java
@@ -0,0 +1,13 @@
+package com.example.ToRunMvnPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToRunMvnProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToRunMvnProApplication.class, args);
+	}
+
+}
